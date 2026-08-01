e8ae8e26553deb4fa1f6894083cbb7dc7580b377
killeromg007 <killeromg003@gmail.com>
2026-08-01 20:46:23 +0800

    Update aim.py

--- util/aim.py
+++ util/aim.py
@@ -25,22 +25,23 @@ def calculate_angles(local_pos, target_pos):
         yaw += 360.0
 
     return pitch, yaw
-origin_x = read_float(pm, local_player_pawn + Offsets.m_vOldOrigin)
-origin_y = read_float(pm, local_player_pawn + Offsets.m_vOldOrigin + 0x4)
-origin_z = read_float(pm, local_player_pawn + Offsets.m_vOldOrigin + 0x8)
+def aimbot(pm,client_base)
+    origin_x = read_float(pm, local_player_pawn + Offsets.m_vOldOrigin)
+    origin_y = read_float(pm, local_player_pawn + Offsets.m_vOldOrigin + 0x4)
+    origin_z = read_float(pm, local_player_pawn + Offsets.m_vOldOrigin + 0x8)
 
-# Read view offset Z component (usually around 64.0 units when standing)
-view_offset_z = read_float(pm, local_player_pawn + Offsets.m_vecViewOffset + 0x8)
+    # Read view offset Z component (usually around 64.0 units when standing)
+   view_offset_z = read_float(pm, local_player_pawn + Offsets.m_vecViewOffset + 0x8)
 
-local_eye_pos = (origin_x, origin_y, origin_z + view_offset_z)
+    local_eye_pos = (origin_x, origin_y, origin_z + view_offset_z)
 
 # Calculate angles to enemy head
-pitch, yaw = calculate_angles(local_eye_pos, head_pos)
+    pitch, yaw = calculate_angles(local_eye_pos, head_pos)
 
 # Write to client view angles
 # View angles usually consist of [pitch, yaw, roll (always 0)]
 # Ensure you have dwViewAngles defined in your offset.json
-view_angles_base = read_ulong64(pm, client_base + Offsets.dwViewAngles)
-if view_angles_base:
-    pm.write_float(view_angles_base + 0x0, pitch)
-    pm.write_float(view_angles_base + 0x4, yaw)
+    view_angles_base = read_ulong64(pm, client_base + Offsets.dwViewAngles)
+    if view_angles_base:
+        pm.write_float(view_angles_base + 0x0, pitch)
+        pm.write_float(view_angles_base + 0x4, yaw)
